312f83f60cec38378fba850ff02a6fde523bc071
rezasetyawan <setyawanreza960@gmail.com>
2024-02-15 22:14:53 +0700

    delete cookie-parser package

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:3001'] });
-  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
