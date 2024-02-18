22f79acbb09db7ebb25e16484c61e3f77fc6701a
rezasetyawan <setyawanreza960@gmail.com>
2024-02-18 13:57:25 +0700

    feat(academy applications): post endpoint

--- src/academy_applications/academy_applications.controller.ts
+++ src/academy_applications/academy_applications.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AcademyApplicationsService } from './academy_applications.service';
+import { Request } from 'express';
+import { CreateAcademyApplicationDto } from './dto/create-academy-application.dto';
 
 @Controller('academy-applications')
 export class AcademyApplicationsController {
-  constructor(private readonly academyApplicationsService: AcademyApplicationsService) {}
+  constructor(
+    private readonly academyApplicationsService: AcademyApplicationsService,
+  ) {}
+
+  @Post()
+  create(
+    @Body() createAcademyApplicationDto: CreateAcademyApplicationDto,
+    @Req() request: Request,
+  ) {
+    const { authorization } = request.headers;
+    if (!authorization) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+    const accessToken = authorization.split(' ')[1];
+
+    if (!accessToken) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+    return this.academyApplicationsService.create(
+      createAcademyApplicationDto,
+      accessToken,
+    );
+  }
 }
--- src/academy_applications/academy_applications.service.ts
+++ src/academy_applications/academy_applications.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as schema from '../drizzle/schema';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { PG_CONNECTION } from 'src/constants';
+import { JwtService } from '@nestjs/jwt';
+import { CreateAcademyApplicationDto } from './dto/create-academy-application.dto';
+import { nanoid } from 'nanoid';
+import { eq } from 'drizzle-orm';
 @Injectable()
-export class AcademyApplicationsService {}
+export class AcademyApplicationsService {
+  constructor(
+    @Inject(PG_CONNECTION) private db: NodePgDatabase<typeof schema>,
+    private jwtService: JwtService,
+  ) {}
+
+  async create(
+    createAcademyApplicationDto: CreateAcademyApplicationDto,
+    accessToken: string,
+  ) {
+    const isTokenValid = await this.jwtService.verifyAsync(accessToken);
+
+    if (!isTokenValid) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    const user = this.jwtService.decode(accessToken);
+
+    const academy = await this.db
+      .select({ id: schema.academies.id })
+      .from(schema.academies)
+      .where(eq(schema.academies.id, createAcademyApplicationDto.academyId));
+
+    if (!academy.length) {
+      throw new NotFoundException('Academy not found');
+    }
+
+    const timestamp = Date.now().toString();
+    const payload = {
+      id: nanoid(40),
+      userId: user.sub as string,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+      ...createAcademyApplicationDto,
+    };
+
+    await this.db.insert(schema.academyApplications).values(payload);
+
+    return {
+      status: 'success',
+    };
+  }
+}
