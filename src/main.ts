dc59117d3d18b6327b16fd5be9706b00d2e0fc26
rezasetyawan <setyawanreza960@gmail.com>
2024-02-22 13:08:00 +0700

    enalble cors for all origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3001'] });
+  app.enableCors({ origin: true });
   await app.listen(3000);
 }
 bootstrap();
